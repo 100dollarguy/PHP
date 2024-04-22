6dacafa4c6e90b016374ac24fc402d1661b9d949
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-23 00:21:09 +0530

    Added prog 6

---
+++ Prog6.php
@@ -0,0 +1,30 @@
+//Write a PHPscript to check whether given number is palindrome or not.
+
+<?php
+
+function isPalindrome($number) {
+    $originalNumber = $number;
+    $reverseNumber = 0;
+    
+    while ($number > 0) {
+        $remainder = $number % 10;
+        $reverseNumber = $reverseNumber * 10 + $remainder;
+        $number = (int)($number / 10);
+    }
+    
+    if ($originalNumber == $reverseNumber) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$number = 12321; // Change this number to check for different numbers
+if (isPalindrome($number)) {
+    echo "$number is a palindrome.";
+} else {
+    echo "$number is not a palindrome.";
+}
+
+?>
+
