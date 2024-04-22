494c8ee2dd42bb233860f17338a2d8dbb5a2b685
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-22 15:04:51 +0530

    Update Prog1.php

--- Prog1.php
+++ Prog1.php
@@ -1,6 +1,13 @@
 //Write a PHP script to print "hello world."
 
+<!DOCTYPE html>
+<html>
+<head>
+<title> Hello World Program </title>
+</head>
+<body>
 <?php
 echo "Hello World!";
 ?>
-
+</body>
+</html>
