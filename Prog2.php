41b3e0052f8b84fb78f1f0264dcfd715f742a62c
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-22 14:31:18 +0530

    Added prog 2,3,4,5

---
+++ Prog2.php
@@ -0,0 +1,11 @@
+<?php
+$x = 26;
+echo "Input Number: ", $x, "\n";
+
+if ($x % 2 == 0) {
+    echo "The input number is even.\n";
+} else {
+    echo "The input number is odd.\n";
+}
+?>
+
---
+++ Prog4.php
@@ -0,0 +1,18 @@
+<?php
+$a = 15;
+$b = 27;
+
+// Displaying the numbers before swapping
+echo "Before swapping:\n";
+echo "Number a = $a and b = $b\n";
+
+// Swap Logic
+$temp = $a;
+$a = $b;
+$b = $temp;
+
+// Displaying the numbers after swapping
+echo "After swapping:\n";
+echo "Number a = $a and b = $b\n";
+?>
+
