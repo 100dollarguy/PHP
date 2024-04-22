6014e5bfd99afe7ee61a66e369855eb7ef863e12
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-22 14:26:04 +0530

    Added prog 1

--- Prog1.php
+++ Prog1.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Hello World Program</title>
-</head>
-<body>
-    <?php
-    echo "Hello World!";
-    ?>
-</body>
-</html>
+<?php
+echo "Hello World!";
+?>
 
