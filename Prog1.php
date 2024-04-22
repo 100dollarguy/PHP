e00fa575fdc79d996aea4a730819e00c01030d58
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-22 15:09:12 +0530

    Update Prog1.php

--- Prog1.php
+++ Prog1.php
@@ -1,3 +1,34 @@
+/*
+
+# Hello World PHP Program
+
+This repository contains a simple PHP program that prints "Hello World!".
+
+## Instructions:
+
+1. **Download XAMPP**:
+   - Download XAMPP from [here](https://www.apachefriends.org/index.html).
+
+2. **Install XAMPP**:
+   - Follow the installation instructions provided by the installer.
+   - Make sure to select Apache and PHP during the installation process.
+
+3. **Start Apache Server**:
+   - Open the XAMPP Control Panel and start the Apache server.
+
+4. **Place PHP File**:
+   - Place the `index.php` file in the `htdocs` directory inside the XAMPP installation directory (usually located at `C:\xampp\htdocs`).
+
+5. **Access PHP File**:
+   - Open your web browser and enter `http://localhost/index.php` in the address bar.
+
+6. **View Output**:
+   - You should see "Hello World!" displayed in your web browser.
+
+*/
+
+
+
 //Write a PHP script to print "hello world."
 
 <!DOCTYPE html>
