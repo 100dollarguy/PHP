02c680e37d9a8580d6ad2e02d3e4c8b946fcf9df
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-22 15:13:46 +0530

    Update Prog1.php

--- Prog1.php
+++ Prog1.php
@@ -1,5 +1,4 @@
 /*
-
 Step-by-Step Guide:
 
 	1.	Download XAMPP:
@@ -41,38 +40,6 @@ http://localhost/index.php
 	•	This will load your index.php file through the local Apache server.
 	8.	View Output:
 	•	You should now see “Hello World!” displayed in your web browser.
-
-readme.md File:
-
-You can create a readme.md file in your project directory with the following content:
-
-# Hello World PHP Program
-
-This repository contains a simple PHP program that prints "Hello World!".
-
-## Instructions:
-
-1. **Download XAMPP**:
-   - Download XAMPP from [here](https://www.apachefriends.org/index.html).
-
-2. **Install XAMPP**:
-   - Follow the installation instructions provided by the installer.
-   - Make sure to select Apache and PHP during the installation process.
-
-3. **Start Apache Server**:
-   - Open the XAMPP Control Panel and start the Apache server.
-
-4. **Place PHP File**:
-   - Place the `index.php` file in the `htdocs` directory inside the XAMPP installation directory (usually located at `C:\xampp\htdocs`).
-
-5. **Access PHP File**:
-   - Open your web browser and enter `http://localhost/index.php` in the address bar.
-
-6. **View Output**:
-   - You should see "Hello World!" displayed in your web browser.
-
-Save this content in a file named readme.md in your project directory. This file will provide instructions for running the PHP program to anyone who accesses your project repository.
-
 */
 
 
