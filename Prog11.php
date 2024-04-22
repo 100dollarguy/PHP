65756576551c4110fe82358c2a3b89384f8671b9
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-04-23 00:30:08 +0530

    Added prog 11

---
+++ Prog11.php
@@ -0,0 +1,24 @@
+//Write a PHP script to implement constructor and destructor
+
+<?php
+
+class Example {
+    // Constructor
+    public function __construct() {
+        echo "Constructor called.\n";
+    }
+
+    // Destructor
+    public function __destruct() {
+        echo "Destructor called.\n";
+    }
+}
+
+// Create an instance of the class
+$example = new Example();
+
+// The object is now out of scope and will be destroyed
+echo "End of script.\n";
+
+?>
+
