1c92777ceb305bb7990294095ed42099bf95dc0e
ROHAN D R <121315563+100dollarguy@users.noreply.github.com>
2024-06-11 10:47:35 +0530

    Modified prog18.php code

--- Prog18.php
+++ Prog18.php
@@ -1,29 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>File Upload Form</title>
-</head>
-<body>
-    <h1>File Upload Form</h1>
-    <form method="post" enctype="multipart/form-data">
-        <label for="file">Select a file to upload:</label>
-        <input type="file" name="file" id="file">
-        <input type="submit" name="submit" value="Upload File">
-    </form>
+<?php
+if(isset($_FILES['image'])) {
+    $errors = array();
+    $file_name = $_FILES['image']['name'];
+    $file_size = $_FILES['image']['size'];
+    $file_tmp = $_FILES['image']['tmp_name'];
+    $file_type = $_FILES['image']['type'];
+    $file_ext = strtolower(end(explode('.', $file_name))); // Fixed the variable name here
+    $allowed_extensions = array("jpeg", "jpg", "png");
 
-    <?php
-    if (isset($_POST['submit'])) {
-        $file = $_FILES['file']['name'];
-        $fileTmpName = $_FILES['file']['tmp_name'];
-        // Specify the directory where the file will be uploaded
-        $uploadDirectory = "/Users/grery/Documents/XAMPP/htdocs/TestFolder/";
-        // Move the uploaded file to the specified directory
-        if (move_uploaded_file($fileTmpName, $uploadDirectory . $file)) {
-            echo "<p>File uploaded successfully: $file</p>";
+    if(!in_array($file_ext, $allowed_extensions)) {
+        $errors[] = "Extension not allowed. Please choose a JPEG or PNG file.";
+    }
+    if($file_size > 2097152) { // 2 MB
+        $errors[] = 'File size must be less than or equal to 2 MB';
+    }
+
+    if(empty($errors)) {
+        $upload_dir = "./images/"; // Directory to save uploaded images
+        $file_destination = $upload_dir . $file_name;
+        if(move_uploaded_file($file_tmp, $file_destination)) {
+            echo "Success";
         } else {
-            echo "<p>Failed to upload file.</p>";
+            $errors[] = "Failed to move uploaded file.";
         }
     }
-    ?>
+
+    if(!empty($errors)) {
+        foreach($errors as $error) {
+            echo $error . "<br>";
+        }
+    }
+}
+?>
+<html>
+<body>
+<form action="" method="POST" enctype="multipart/form-data">
+    <input type="file" name="image" />
+    <input type="submit" value="Upload" />
+</form>
 </body>
 </html>
